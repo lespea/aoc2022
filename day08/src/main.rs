3db1f2c4df8edd551618ba06610b74472121106a
Adam Lesperance <lespea@gmail.com>
2022-12-09 00:29:44 -0600

    Finish day08

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use ndarray::{Array, ArrayBase, OwnedRepr};
 
 const PART_1: bool = false;
 
@@ -13,43 +13,183 @@ fn main() {
 }
 
 fn part1() {
-    println!("{}", solve(PART_1_DATA, 4));
+    println!("{}", Forest::new(PART_1_DATA).count_vis());
 }
 
 fn part2() {
-    println!("{}", solve(PART_1_DATA, 14));
+    println!("{}", Forest::new(PART_1_DATA).scenic_score());
 }
 
-fn solve(data: &str, group_size: usize) -> usize {
-    let mut dupes = HashSet::with_capacity(group_size * 2);
+#[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Debug)]
+struct Tree {
+    height: u8,
+    visible: bool,
+    scenic_view: usize,
+}
 
-    'WIND: for (idx, wind) in data.as_bytes().windows(group_size).enumerate() {
-        dupes.clear();
+struct Forest {
+    trees: ArrayBase<OwnedRepr<Tree>, ndarray::Dim<[usize; 2]>>,
+}
 
-        for b in wind.iter() {
-            if !dupes.insert(*b) {
-                continue 'WIND;
+impl Forest {
+    pub fn new(data: &str) -> Self {
+        let size = data.lines().next().unwrap().len();
+
+        let trees: Vec<Tree> = data
+            .lines()
+            .flat_map(|line| {
+                line.bytes().map(|b| Tree {
+                    height: b - b'0',
+                    visible: false,
+                    scenic_view: 0,
+                })
+            })
+            .collect();
+
+        let mut forest = Forest {
+            trees: Array::from_shape_vec((size, size), trees).unwrap(),
+        };
+
+        forest.calc_vis();
+        forest.update_scenic_score();
+
+        forest
+    }
+
+    // fn reset_vis(&mut self) {
+    //     for tree in self.trees.iter_mut() {
+    //         tree.visible = false;
+    //     }
+    // }
+
+    fn calc_vis(&mut self) {
+        for mut row in self.trees.rows_mut() {
+            let mut max = None;
+            for tree in row.iter_mut() {
+                Forest::adjust(&mut max, tree);
+            }
+
+            max = None;
+            for tree in row.iter_mut().rev() {
+                Forest::adjust(&mut max, tree);
             }
         }
 
-        return idx + group_size;
+        for mut col in self.trees.columns_mut() {
+            let mut max = None;
+            for tree in col.iter_mut() {
+                Forest::adjust(&mut max, tree);
+            }
+
+            max = None;
+            for tree in col.iter_mut().rev() {
+                Forest::adjust(&mut max, tree);
+            }
+        }
     }
-    panic!("no answer")
+
+    fn adjust(max: &mut Option<u8>, tree: &mut Tree) {
+        match max {
+            Some(h) => {
+                if tree.height > *h {
+                    tree.visible = true;
+                    max.replace(tree.height);
+                }
+            }
+
+            None => {
+                tree.visible = true;
+                max.replace(tree.height);
+            }
+        }
+    }
+
+    pub fn update_scenic_score(&mut self) {
+        let t = &mut self.trees;
+        let n = t.nrows();
+
+        for col in 0..n {
+            for row in 0..n {
+                let height = t.get((row, col)).unwrap().height;
+
+                let mut left = 0;
+                let mut up = 0;
+                let mut right = 0;
+                let mut down = 0;
+
+                for c in (0..col).rev() {
+                    left += 1;
+                    if t.get((row, c)).unwrap().height >= height {
+                        break;
+                    }
+                }
+                for c in (col + 1)..n {
+                    right += 1;
+                    if t.get((row, c)).unwrap().height >= height {
+                        break;
+                    }
+                }
+
+                for r in (0..row).rev() {
+                    up += 1;
+                    if t.get((r, col)).unwrap().height >= height {
+                        break;
+                    }
+                }
+                for r in (row + 1)..n {
+                    down += 1;
+                    if t.get((r, col)).unwrap().height >= height {
+                        break;
+                    }
+                }
+
+                t.get_mut((row, col)).unwrap().scenic_view = left * up * right * down;
+            }
+        }
+    }
+
+    pub fn count_vis(&self) -> usize {
+        self.trees.iter().filter(|t| t.visible).count()
+    }
+
+    pub fn scenic_score(&self) -> usize {
+        self.trees.iter().map(|t| t.scenic_view).max().unwrap()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    use std::fmt::Write;
+
+    fn test_data() -> &'static str {
+        "30373
+25512
+65332
+33549
+35390"
+    }
+
     #[test]
     fn examples() {
-        for (data, len, want) in [
-            ("bvwbjplbgvbhsrlpgdmjqwftvncz", 4, 5),
-            ("nppdvjthqldpwncqszvftbrmjlhg", 4, 6),
-            ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4, 10),
-            ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4, 11),
-        ] {
-            assert_eq!(want, solve(data, len));
+        let f = Forest::new(test_data());
+
+        let mut h = String::with_capacity(10);
+        let mut s = String::with_capacity(10);
+
+        for row in f.trees.rows() {
+            for t in row.iter() {
+                write!(&mut h, "{}", t.height).unwrap();
+                write!(&mut s, "{}", t.scenic_view).unwrap();
+            }
+            h.write_char('\n').unwrap();
+            s.write_char('\n').unwrap();
         }
+
+        println!("{}\n\n{}", h, s);
+
+        assert_eq!(21, f.count_vis());
+        assert_eq!(8, f.scenic_score())
     }
 }
