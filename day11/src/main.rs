bfe7a585cfd7e4c7aa8c2112c0c3f064baae5ac0
Adam Lesperance <lespea@gmail.com>
2022-12-11 10:35:30 -0600

    Add day 11 stub

---
+++ day11/src/main.rs
@@ -0,0 +1,23 @@
+const PART_1: bool = false;
+
+static PART_1_DATA: &str = include_str!("input");
+
+fn main() {
+    if PART_1 {
+        part1();
+    } else {
+        part2();
+    }
+}
+
+fn part1() {}
+
+fn part2() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {}
+}
